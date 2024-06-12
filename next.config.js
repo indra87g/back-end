beb5074f291b225c6eaba6483023af5aebbf0be0
Indra Sah Noeldy <twinightwheel50@gmail.com>
2024-06-12 09:26:58 +0700

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const { title } = require('process');
+
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+module.exports = withNextra({});
---
+++ theme.config.jsx
@@ -0,0 +1,30 @@
+export default {
+    logo: (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <img src="/logo.png" width="35" />
+            <span style={{ marginLeft: 10, fontWeight: 600 }}>belajarfrontend.org</span>
+        </div>
+    ),
+    project: {
+        link: 'https://github.com/nauvalazhar/front-end',
+    },
+    docsRepositoryBase: 'https://github.com/nauvalazhar/front-end/tree/main',
+    toc: {
+        title: 'Dalam Bab Ini',
+        backToTop: true
+    },
+    editLink: {
+        text: 'Edit halaman ini →',
+    },
+    feedback: {
+        content: null,
+    },
+    footer: {
+        text: 'Nauval © 2024',
+    },
+    head: (
+        <>
+            <meta name="shortcut icon" content="/favicon.ico" />
+        </>
+    )
+}
